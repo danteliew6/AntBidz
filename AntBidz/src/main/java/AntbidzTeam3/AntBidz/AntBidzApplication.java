ba8d210180c8ad629932a96df29a4e3b47021a7d
danteliew6 <danteliew6@gmail.com>
2021-03-14 20:53:41 +0800

    Clone project to repository

---
+++ AntBidz/src/main/java/AntbidzTeam3/AntBidz/AntBidzApplication.java
@@ -0,0 +1,13 @@
+package AntbidzTeam3.AntBidz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntBidzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntBidzApplication.class, args);
+	}
+
+}
---
+++ AntBidz/src/test/java/AntbidzTeam3/AntBidz/AntBidzApplicationTests.java
@@ -0,0 +1,13 @@
+package AntbidzTeam3.AntBidz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntBidzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
